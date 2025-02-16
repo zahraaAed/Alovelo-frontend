c042297851b8f074218729d32d6ecbd0d6d3c9c0
zahraaAed <zahraaalaaeddine94@gmail.com>
2025-02-16 16:36:05 +0200

    home css

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+import Header from "@/Component/Header.jsx";
+import Link from "next/link";
 
 export default function Home() {
   const [homeData, setHomeData] = useState(null);
@@ -20,31 +24,45 @@ export default function Home() {
       <Head>
         <title>Alovelo Home</title>
       </Head>
-
+      <Header />
       {/* Hero Section */}
-      <section className="hero-section">
-        <img src={`http://localhost:5000${homeData.imageHome}`} alt="Hero Image" class="hero-image"/>
-        <h1 className="hero-text">{homeData.subtitlehomeHeader}</h1>
+      <section className={styles.heroSection}>
+      <Image 
+  src={`http://localhost:5000${homeData.imageHome}`} 
+  alt="Hero Image" 
+  width={1000}
+  height={300} 
+  className={styles.heroImage} 
+/>
+<div className={styles.hero}>
+<div className={styles.heroContent}>
+        <h1>{homeData.subtitlehomeHeader}</h1>
         <p className="hero-text">{homeData.contenthomeHeader}</p>
-        <button  class="boutton">{homeData.ctabtnhomeHeader}</button>
+        <Link href="/about" passHref>
+  <button className={styles.boutton}>
+    {homeData.ctabtnhomeHeader}
+  </button>
+</Link>
+        </div>
+        </div>
       </section>
 
       {/* Events Section */}
-      <section class="events-section" id="events">
-        <img src={`http://localhost:5000${homeData.imageSection}`} alt="Event" class="event-image"  />
+      <section class={styles.eventSection} >
+        <img src={`http://localhost:5000${homeData.imageSection}`} alt="Event" className={styles.eventImage}  />
         {/*<h2>{homeData.titleSection}</h2>*/}
-        <p class="events-text">{homeData.contentSection}</p>
+        <p className={styles.eventstext} >{homeData.contentSection}</p>
       </section>
 
       {/* Promotional Section */}
-      <section >
+      <section className={styles.promotion}>
         <h2>{homeData.titlePromotionalSection}</h2>
-        <button class="boutton">{homeData.ctabtnPromotionalSection}</button>
+        <button >{homeData.ctabtnPromotionalSection}</button>
       </section>
 
       {/* Gallery */}
-      <section class="gallery">
-        <div class="gallery-container">
+      <section className={styles.gallery}>
+        <div className={styles.galleryContainer}>
           {homeData.galleryImages.map((img, index) => (
             <img key={index} src={`http://localhost:5000${img}`} alt={`Gallery ${index}`} width="300px" />
           ))}
@@ -52,9 +70,7 @@ export default function Home() {
       </section>
 
       {/* Footer */}
-      <footer style={{ backgroundColor: "#333", color: "#fff", padding: "20px", textAlign: "center" }}>
-        <p>Copyright © All Rights Reserved</p>
-      </footer>
+      
     </div>
   );
 }
